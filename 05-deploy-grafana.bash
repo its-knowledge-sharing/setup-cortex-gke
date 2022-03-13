4443ca348ddfaaef56840f2c1f8f75c81c715c5b
seubpong.mon <pjame.fb@gmail.com>
2022-03-13 15:42:45 +0700

    Deploy grafana, skip test

--- 05-deploy-grafana.bash
+++ 05-deploy-grafana.bash
@@ -9,6 +9,7 @@ helm repo add grafana-helm https://grafana.github.io/helm-charts
 
 helm template grafana grafana-helm/grafana \
 -f grafana/grafana.yaml \
+--skip-tests \
 --namespace ${NS} > tmp-grafana-cortex.yaml
 
 kubectl apply -n ${NS} -f tmp-grafana-cortex.yaml
