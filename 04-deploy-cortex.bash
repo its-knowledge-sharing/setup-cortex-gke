f7a4ce078737e4703161b0a0437d491e583d794b
seubpong.mon <pjame.fb@gmail.com>
2022-03-13 13:56:32 +0700

    Added helm cortex

--- 04-deploy-cortex.bash
+++ 04-deploy-cortex.bash
@@ -4,7 +4,9 @@ source .env
 
 helm repo add cortex-helm https://cortexproject.github.io/cortex-helm-chart
 
-helm template cortex-helm/cortex \
+helm template cortex cortex-helm/cortex \
+-f cortex/cortex.yaml \
 --set config.blocks_storage.gcs.bucket_name=${BUCKET_NAME} \
 --set config.ruler_storage.gcs.bucket_name=${BUCKET_NAME} \
---set config.alertmanager_storage.gcs.bucket_name=${BUCKET_NAME} > tmp-cortex.yaml
+--set config.alertmanager_storage.gcs.bucket_name=${BUCKET_NAME} \
+--namespace cortex > tmp-cortex.yaml
