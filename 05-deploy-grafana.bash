a21c080355e51ac89c2847ca218333cf327fe953
seubpong.mon <pjame.fb@gmail.com>
2022-03-13 15:40:55 +0700

    Deploy grafana

---
+++ 05-deploy-grafana.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source .env
+
+NS=grafana-cortex
+kubectl create ns ${NS}
+
+helm repo add grafana-helm https://grafana.github.io/helm-charts
+
+helm template grafana grafana-helm/grafana \
+-f grafana/grafana.yaml \
+--namespace ${NS} > tmp-grafana-cortex.yaml
+
+kubectl apply -n ${NS} -f tmp-grafana-cortex.yaml
+
+kubectl apply -n ${NS} -f grafana/grafana-ing.yaml
