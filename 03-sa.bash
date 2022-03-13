3ea535abe25d3e574cbf9506027c45e40a00ae88
seubpong.mon <pjame.fb@gmail.com>
2022-03-13 13:02:36 +0700

    Added project SA policy binding

--- 03-sa.bash
+++ 03-sa.bash
@@ -3,3 +3,7 @@
 source .env
 
 gcloud iam service-accounts create ${SA_NAME} --display-name="Service account for Cortex"
+
+gcloud projects add-iam-policy-binding ${PROJECT} \
+    --member="serviceAccount:${SA_NAME}@${PROJECT}.iam.gserviceaccount.com" \
+    --role="roles/storage.objectAdmin"
