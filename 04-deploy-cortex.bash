87c68a0b91f0dd3003acfcea777aee0e6d098927
seubpong.mon <pjame.fb@gmail.com>
2022-03-13 15:26:47 +0700

    Create ingress

--- 04-deploy-cortex.bash
+++ 04-deploy-cortex.bash
@@ -7,6 +7,8 @@ KEY_FILE=sm.json
 SA=${SA_NAME}@${PROJECT}.iam.gserviceaccount.com
 SECRET=gcp-sa
 
+kubectl create ns ${NS}
+
 # Create service account secret
 gcloud iam service-accounts keys create ${KEY_FILE} --iam-account=${SA}
 kubectl delete secret ${SECRET} -n ${NS}
@@ -21,8 +23,6 @@ helm template cortex cortex-helm/cortex \
 --set config.ruler_storage.gcs.bucket_name=${BUCKET_NAME} \
 --set config.alertmanager_storage.gcs.bucket_name=${BUCKET_NAME} \
 --namespace ${NS} > tmp-cortex.yaml
-
-kubectl create ns ${NS}
 
 kubectl apply -n ${NS} -f tmp-cortex.yaml
 
